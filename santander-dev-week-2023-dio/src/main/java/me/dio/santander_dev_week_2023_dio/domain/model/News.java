ae1247bf6d773204ffc7541b8f5851e6456de8b6
jorge <jorgearmandovdj@gmail.com>
2025-03-22 00:23:30 -0300

    removed santender-dev-week-2023

--- santander-dev-week-2023-dio/src/main/java/me/dio/santander_dev_week_2023_dio/domain/model/News.java
+++
@@ -1,8 +0,0 @@
-package me.dio.santander_dev_week_2023_dio.domain.model;
-
-import jakarta.persistence.Entity;
-
-@Entity(name = "tb_news")
-public class News extends BaseItem {
-
-}
--- santander-dev-week-2023-dio/src/main/java/me/dio/santander_dev_week_2023_dio/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package me.dio.santander_dev_week_2023_dio.service;
-
-import me.dio.santander_dev_week_2023_dio.domain.model.User;
-
-public interface UserService {
-    User findById(Long id);
-    User create(User user);
-}
